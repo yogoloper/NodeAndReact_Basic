be049e54aaf44d4b33dc864f8825316d0364e596
yogoloper <jguddnjs@gmail.com>
2022-03-23 23:29:33 +0900

    비밀 설정 정보 관리 추가

--- Index.js
+++ Index.js
@@ -1,39 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const bodyParser = require('body-parser')
-const { User } = require('./models/User')
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDb Connected..'))
+  .catch((err) => console.log(err));
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://root:1234@cluster0.jggys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log("MongoDb Connected.."))
-  .catch(err => console.log(err))
-
-app.get('/', (req, res) => { 
-  res.send('Hello World!')
-})
+app.get('/', (req, res) => {
+  res.send('Hello World! a');
+});
 
 app.post('/register', (req, res) => {
   // 회원가입 할대 필요한 정보 client에서 가져오면
   // 그것들을 DB에 넣어준다.
-  const user = new User(req.body)
+  const user = new User(req.body);
 
   user.save((err, userInfo) => {
-    if(err) return res.json({ success: false, err})
+    if (err) return res.json({ success: false, err });
 
     return res.status(200).json({
-      success: true
-    })
-  })
-})
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
